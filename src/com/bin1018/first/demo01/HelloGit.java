d73a6434ea512b8ff16266e14aca0bb1b7780506
zhubin1018 <2470718490@qq.com>
2020-07-01 21:40:34 +0800

    更新1

--- src/com/bin1018/first/demo01/HelloGit.java
+++ src/com/bin1018/first/demo01/HelloGit.java
@@ -22,12 +22,13 @@ public class HelloGit {
 
 	/**
 	 * @Title: main @Description: TODO(这里用一句话描述这个方法的作用) @param @param args
-	 * 参数 @return void 返回类型 @throws
+	 *         参数 @return void 返回类型 @throws
 	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloGit");
+		System.out.println("更新1");
 
 	}
 
