a7c50d6750cb8dd479d8247a31ab73ae140073e3
zhubin1018 <2470718490@qq.com>
2020-07-01 21:35:15 +0800

    shufu

---
+++ src/com/bin1018/first/demo01/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.bin1018.first.demo01;
+
+/*
+ * ³£ÓÃ¿ì½Ý¼ü 	
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
