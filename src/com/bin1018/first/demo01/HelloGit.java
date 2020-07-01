0fb596dfd8296a33b270f3b3104a716b2470d21c
zhubin1018 <2470718490@qq.com>
2020-07-01 21:37:43 +0800

    更新

---
+++ src/com/bin1018/first/demo01/HelloGit.java
@@ -0,0 +1,34 @@
+
+/**  
+ * @Title: HelloGit.java
+ * @Package com.bin1018.first.demo01
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author ZHUBIN
+ * @date 2020年7月1日 下午9:36:21 
+ * @version V1.0  
+ */
+
+package com.bin1018.first.demo01;
+
+/**
+ * @ClassName: HelloGit
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author ZHUBIN
+ * @date 2020年7月1日 下午9:36:21
+ *
+ */
+
+public class HelloGit {
+
+	/**
+	 * @Title: main @Description: TODO(这里用一句话描述这个方法的作用) @param @param args
+	 * 参数 @return void 返回类型 @throws
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloGit");
+
+	}
+
+}
